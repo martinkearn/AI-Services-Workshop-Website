a90948ea733d3e5135c798c240aa3f3ded9dbf70
Martin Kearn <martin.kearn@microsoft.com>
2018-03-12 17:07:59 +0000

    Links on home page

--- AISW-Web/Controllers/HomeController.cs
+++ AISW-Web/Controllers/HomeController.cs
@@ -5,14 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AISW_Web.Models;
+using AISW_Web.Interfaces;
 
 namespace AISW_Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IStoreRepository _storeRepository;
+
+        public HomeController(IStoreRepository storeRepository)
         {
-            return View();
+            _storeRepository = storeRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var links = await _storeRepository.GetLinks();
+
+            return View(links);
         }
 
         public IActionResult Error()
